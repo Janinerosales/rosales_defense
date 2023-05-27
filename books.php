203e3c5f08a67c313daa335bbf6ca4a70a844878
Janinerosales <rhosalesj@gmail.com>
2023-05-27 10:55:05 +0800

    add book class

--- books.php
+++ books.php
@@ -1,6 +1,17 @@
 <?php
 
-abstract class Books extends Database
+class Books extends Database
+
 {
-    
+    public function db():string
+    {
+        $dbName ="CREATE DATABASE IF NOT EXISTS $this->dbName";
+        
+        return $this->conn->query($dbName);
+    }
 }
+
+$new = new Books();
+$new->db();
+
+?>
--- index.php
+++ index.php
@@ -17,17 +17,3 @@ abstract class Database
     abstract public function db(): string;
 }
 
-class Books extends Database
-
-{
-    public function db():string
-    {
-        $dbName ="CREATE DATABASE IF NOT EXISTS $this->dbName";
-        
-        return $this->conn->query($dbName);
-    }
-}
-
-$new = new Books();
-$new->db();
-
